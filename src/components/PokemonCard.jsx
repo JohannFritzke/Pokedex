c2cb8eaf04ffd17ec23423e0040822796bde464e
JohannFritzke <johannfritzke@hotmail.com>
2023-06-06 00:38:54 -0300

    adição de forma shiny

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,25 +1,43 @@
+import axios from "axios";
 
-
-export default function PokemonCard ({name, img,types,number}){
-    const typesHandler=()=>{
-        if(types[1]){
-            return  <div className="types">
-                        <p className={`type1 ${types[0].type.name}`}>{types[0].type.name}</p>
-                        <p className={`type2 ${types[1].type.name}`}>{types[1].type.name}</p>
-                    </div>
-                    
-        }else{
-            return  <div className="types">
-                     <p className={`type1 ${types[0].type.name}`}>{types[0].type.name}</p>
-                    </div>
-        }   
-    }
-    return(
-        <div className="cardPoke">
-            <p className="number">#{number}</p>
-            <img src={img} alt=""/>
-            <h5>{name}</h5>
-            <p>{typesHandler()}</p>
+export default function PokemonCard({ name, img, types, number }) {
+  const typesHandler = () => {
+    if (types[1]) {
+      return (
+        <div className="types">
+          <p className={`type1 ${types[0].type.name}`}>{types[0].type.name}</p>
+          <p className={`type2 ${types[1].type.name}`}>{types[1].type.name}</p>
         </div>
-    )
+      );
+    } else {
+      return (
+        <div className="types">
+          <p className={`type1 ${types[0].type.name}`}>{types[0].type.name}</p>
+        </div>
+      );
+    }
+  };
+
+  const shiny = (id) => {
+    var shiny = document.getElementById(id);
+
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`).then((resp) => {
+      if(shiny.src.includes("shiny")){
+        shiny.src = resp.data.sprites.other["official-artwork"].front_default;
+      }else if(resp.data.sprites.other["official-artwork"].front_shiny != null ){
+        shiny.src = resp.data.sprites.other["official-artwork"].front_shiny;
+      }
+    });
+
+    //shiny.src=requi.data.sprites.other["official-artwork"].front_shiny
+  };
+  return (
+    <div className="cardPoke">
+      <p className="number">#{number}</p>
+      <img src={img} alt="" id={number} />
+      <h5>{name}</h5>
+      {typesHandler()}
+      <p className="bi bi-star-fill shiny" onClick={() => shiny(number)}></p>
+    </div>
+  );
 }
